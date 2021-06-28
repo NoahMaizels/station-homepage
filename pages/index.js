f085b3917c281ab4e97d77c1ca88dd166090b25e
noahniuwa <noahmaizels@protonmail.com>
2021-06-27 19:02:27 -0600

    new vanta settings

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Index() {
       backgroundColor: '#474782',
       points: 12,
       maxDistance: 24,
-      spacing: matches ? 20 : 32,
+      spacing: matches ? 20 : 40,
       scale: 1,
       scaleMobile: 1
     })
