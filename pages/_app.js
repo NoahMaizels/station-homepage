57a0c281eb6b69ee880f9d17675d7fb4e608ae1b
noahniuwa <noahmaizels@protonmail.com>
2021-07-13 18:26:31 -0600

    new animation

--- pages/_app.js
+++ pages/_app.js
@@ -39,6 +39,7 @@ function MyApp({ Component, pageProps }) {
           href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css"
           rel="stylesheet"></link>
         <script src="/scripts/three.min.js"></script>
+        <script src="/scripts/particles.min.js"></script>
       </Head>
       <Component {...pageProps} />
     </>
